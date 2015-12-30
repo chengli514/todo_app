be0d9ffcb36dba64c998365f837b90e01768796b
Danny <chengli6206@gmail.com>
2015-12-29 22:52:16 -0800

    finished basic CRUD actions

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,16 +3,45 @@ class TodoController < ApplicationController
     @todos = Todo.all
   end
   
+  def show
+    @todo = Todo.find(params[:id])
+  end
+
   def new
+    @todo = Todo.new
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
   end
 
   def create
     @todo = Todo.new(params.require(:todo).permit(:title, :text))
-    @todo.save
-    redirect_to @todo
+    if @todo.save
+      redirect_to @todo
+    else
+      render 'new'
+    end
   end
 
-  def show
+  def update
     @todo = Todo.find(params[:id])
+    if @todo.update(todo_params)
+      redirect_to @todo
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+   
+    redirect_to todo_index_path
+  end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:title, :text)
   end
 end
